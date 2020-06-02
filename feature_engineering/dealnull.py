10d10f8515fd72bc7f6f712804eeb78b3798ea0e
HP <949467994@qq.com>
2020-06-02 21:04:58 +0800

    多列特征提取

--- feature_engineering/dealnull.py
+++ feature_engineering/dealnull.py
@@ -1,14 +1,27 @@
 import json
 from sklearn.feature_selection import VarianceThreshold
+import pandas as pd
 
 def readdata(path):
-    file = open(path, 'r', encoding='utf-8')
-    print(file.readline())
-    print(file.readline())
+    # file = open(path, 'r', encoding='utf-8')
+    # print(file.readline())
 
+    rawdata = pd.read_table(path, iterator=True, header=None)
+    data = pd.DataFrame(rawdata)
+
+    print(type(data))
+
+    # while True:
+    #     try:
+    #         chunk = train_data.get_chunk(5600000)
+    #         chunk.columns = ['user_id', 'spu_id', 'buy_or_not', 'date']
+    #         chunk.to_csv('big_data.csv', mode='a', header=False, index=None)
+    #     except Exception as e:
+    #         break
+    #
 # X = [[0, 0, 1], [0, 1, 0], [1, 0, 0], [0, 1, 1], [0, 1, 0], [0, 1, 1]]
 # sel = VarianceThreshold(threshold=(.8 * (1 - .8)))
 # sel.fit_transform(X)
 
 if __name__ == '__main__':
-    readdata("../task-32323461-stdout")
+    readdata("../rawdata")
--- feature_statistics/raw.py
+++ feature_statistics/raw.py
@@ -3,7 +3,7 @@ import pandas as pd
 import json
 import csv
 
-#连续
+#连续,速度慢
 def readrawdata(inputpath):
     feature = "videoDuration"
     df = pd.DataFrame()
@@ -19,32 +19,11 @@ def readrawdata(inputpath):
             df = df.append(data_df, ignore_index = True)
     print(df)
     file.close()
-
-def transcsv(inputpath , feature="videoDuration", output="./raw_2.csv"):
+def transcsv_D(inputpath , output="./raw.csv"):
     csv_file = open(output, 'w', encoding='utf-8',newline="")  # 默认newline='\n'
     writer = csv.writer(csv_file)
-    writer.writerow([feature])
-    miss = 0
-    number = 0
-    with open(inputpath, 'r', encoding='utf-8') as file:
-        for line in file.readlines():
-            data = json.loads(line)
-            number = number + 1
-            try:
-                value = data['featureIntMap'][feature]
-            except:
-                value = None
-                miss = miss + 1
-            writer.writerow([value])
-    csv_file.close()
-    file.close()
-    print(miss)
-    print(number)
-
-def transcsv_D(inputpath , feature="age", output="./raw.csv"):
-    csv_file = open(output, 'w', encoding='utf-8',newline="")  # 默认newline='\n'
-    writer = csv.writer(csv_file)
-    featurelist = ["deviceId", feature]
+    featurelist = ["itemId", "videoCategory", "sourceTags", "videoSize", "nlpCategory", "videoSubCategory", "videoWidth", "videoHeight"]
+    #featurelist = ["deviceId", "age","gender","country","province","city","hourOfDay","dayOfWeek","dayOfMonth","minute","model"]
     writer.writerow(featurelist)
     number = 0
     miss = 0
@@ -57,18 +36,18 @@ def transcsv_D(inputpath , feature="age", output="./raw.csv"):
             value = list()
             for feature in featurelist:
                 try:
-                    value.append(data['commonStrMap'][feature])
+                    value.append(data['featureStrMap'][feature])
+                    #value.append(data['commonStrMap'][feature])
                 except:
                     value.append(None)
-                    miss = miss + 1
+                    #miss = miss + 1
             writer.writerow(value)
     csv_file.close()
     file.close()
     #原始数据特征缺失值
-    print(miss)
-    print(number)
-
-def readcsv2(input="./raw.csv",feature='age'):
+    #print(miss)
+    #print(number)
+def readcsv2(input="./raw.csv"):
     reader = pd.read_csv(input, iterator=True)
     loop = True
     chunksize = 6000
@@ -76,14 +55,16 @@ def readcsv2(input="./raw.csv",feature='age'):
     while loop:
         try:
             chunk = reader.get_chunk(chunksize)
-            chunk = chunk.drop_duplicates(['deviceId'])
+            #chunk = chunk.drop_duplicates(['deviceId'])
+            chunk = chunk.drop_duplicates(['itemId'])
             chunks.append(chunk)
         except StopIteration:
             loop = False
             print("Iteration is stopped.")
     df = pd.concat(chunks, ignore_index=True)
     df = pd.DataFrame(df)
-    df = df.drop_duplicates(['deviceId']).reset_index(drop=True)
+    df = df.drop_duplicates(['itemId']).reset_index(drop=True)
+    #df = df.drop_duplicates(['deviceId']).reset_index(drop=True)
     #计算去重后的特征缺失值
     miss = df.isnull().sum().tolist()[1]
     number = df.shape[0]
@@ -91,7 +72,12 @@ def readcsv2(input="./raw.csv",feature='age'):
     # print(miss)
     # print(ratio)
     # print(number)
-    df[feature].value_counts().to_csv("./result/" + feature + ".csv", sep="\t", encoding="utf-8")
+    print(df)
+    featurelist = ["itemId", "videoCategory", "sourceTags", "videoSize", "nlpCategory", "videoSubCategory",
+                   "videoWidth", "videoHeight"]
+    #featurelist = ["deviceId", "age","gender","country","province","city","hourOfDay","dayOfWeek","dayOfMonth","minute","model"]
+    for feature in featurelist[1:]:
+        df[feature].value_counts(normalize=True,dropna=False).to_csv("./result/" + feature + ".csv", sep="\t", encoding="utf-8")
 
 import glob, os
 def read_multicsv():
@@ -120,6 +106,31 @@ def read_multicsv():
     df = pd.DataFrame(df.describe(percentiles=[.1, .2, .3, .4, .5, .6, .7, .8, .9]))
     df.to_csv("./out.csv", sep="\t")
 
+def transcsv(inputpath , output="./raw.csv"):
+    csv_file = open(output, 'w', encoding='utf-8',newline="")  # 默认newline='\n'
+    featurelist=["videoDuration","item12Expose","item24Expose","item72Expose","item12Skip","item24Skip","item72Skip","item12Duration","item24Duration","item72Duration","item12View","item24View","item72View","item12Click","item24Click","item72Click","commentInterval","titleLen"]
+    writer = csv.writer(csv_file)
+    writer.writerow(featurelist)
+    miss = 0
+    number = 0
+    with open(inputpath, 'r', encoding='utf-8') as file:
+        for line in file.readlines():
+            number = number + 1
+            if number % 5000 == 0:
+                print(str(number) + " lines")
+            data = json.loads(line)
+            value = list()
+            for feature in featurelist:
+                try:
+                    value.append(data['featureIntMap'][feature])
+                except:
+                    value.append(None)
+                    #miss = miss + 1
+            writer.writerow(value)
+    csv_file.close()
+    file.close()
+    #print(miss)
+    print(number)
 
 def readcsv(input="./raw.csv"):
     reader = pd.read_csv(input,iterator=True, dtype='float')
@@ -140,37 +151,11 @@ def readcsv(input="./raw.csv"):
 
 if __name__ == '__main__':
     #read_multicsv()
-
-    inputpath = './rawdata'
-
-    # start = time.clock()
-    # readrawdata(inputpath)
-    # end = time.clock()
-    # print(end - start)
+    inputpath = '../rawdata'
 
     # transcsv(inputpath)
-    # start = time.clock()
     # readcsv()
-    # end = time.clock()
-    # print(end - start)
-    #
-    # transcsv_D(inputpath)
-    # start = time.clock()
-    # readcsv2()
-    # end = time.clock()
-    # print(end - start)
 
+    transcsv_D(inputpath)
+    readcsv2()
 
-#离散
-# def readrawdata():
-#     dic = {"id":[1,1,2,2,3,4],
-#            "age":[12,12,12,12,23,67],
-#            "city":["b","b","c","c","f","g"]
-#            }
-#     df = pd.DataFrame(dic)
-#     print(df)
-#
-#     df2 = df.drop_duplicates(['id']).reset_index(drop = True)
-#     print(df2)
-#
-#     print(max(dic.values()))
